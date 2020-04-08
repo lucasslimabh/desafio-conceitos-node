cc32199ec4ab0a252a7f7544896c318b17dbecd8
Lucas Soares Lima <34869148+lucasslimabh@users.noreply.github.com>
2020-04-08 16:21:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -27,9 +27,18 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs} = request.body;
 
-  repositories.push({id : uuid(), title, url, techs, likes: 0});
+  const repository = {
+    id : uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0
+  }
 
-  return response.status(200).json(repositories);
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -39,7 +48,7 @@ app.put("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
-    return response.status(400).json({error : 'repository not found'})
+    return response.status(204).json({error : 'repository not found'})
   }
 
   const repository = {
@@ -74,6 +83,10 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const {id} = request.params;
 
+  if (!isUuid(id)) {
+    return response.status(400).json({error : "Invalid id"});
+  }
+
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex  < 0) {
@@ -81,9 +94,9 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   repositories[repositoryIndex].likes =  repositories[repositoryIndex].likes + 1;
-
+  let likes = repositories[repositoryIndex].likes
   
-  return response.json({likes : repositories[repositoryIndex].likes});
+  return response.json({"likes" : likes});
 });
 
 module.exports = app;
